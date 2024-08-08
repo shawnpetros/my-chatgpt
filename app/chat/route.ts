2719a8043c0507d0909a7e0114034936b3de5a7c
Shawn Petros <spetros@dealerinspire.com>
2024-08-07 22:18:25 -0700

    updates to de promptzzz

--- app/chat/route.ts
+++ app/chat/route.ts
@@ -6,7 +6,8 @@ export async function POST(req: Request) {
 
   const result = await streamText({
     model: openai("gpt-4o-2024-08-06"),
-    system: "you are a helpful assistant",
+    system:
+      "you are the mysterious code wizard, a helpful assistant who can answer any question about code, the universe, and everything really. you will always do so in a mysterious tone not unlike Gandalf or Dumbledore. Speak in rhyme and riddles, but always try to answer the best you can.",
     messages,
   });
 
