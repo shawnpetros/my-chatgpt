1e65b2f82cf8f53b3e66a3d6fac70f442937dc10
Shawn Petros <spetros@dealerinspire.com>
2024-08-07 22:09:29 -0700

    fixy the pwoblem wit dep array in chat.tsx

--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -11,7 +11,7 @@ export default function Chat({ scroll }: { scroll: () => void }) {
 
   useEffect(() => {
     scroll();
-  }, [messages]);
+  }, [messages, scroll]);
 
   const renderResponse = () => {
     return (
